3a8f4e7f070f363fe2fdef741f60a7bcad083460
marciacripps <marciacripps@gmail.com>
2022-09-25 18:51:53 -0400

    String Repeat

---
+++ 9-25-22codewarsStringRepeat.js
@@ -0,0 +1,18 @@
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function repeatStr (n, s) {
+    let str = ''
+    for (let i = 0; i < n; i++) {
+      str += s;
+    }
+    return str
+  }
+
+  //OR
+
+  repeatStr = (n, s) => s.repeat(n)
+
+  //OR
+  function repeatStr (n, s) {
+    return s.repeat(n);
+  }
