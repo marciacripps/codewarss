a4c06afca013b69dc4dc943d343d09ca4bd6a4df
marciacripps <marciacripps@gmail.com>
2022-10-04 11:12:30 -0400

    10-4-22 8kyu

---
+++ 10-4-22BMI.js
@@ -0,0 +1,39 @@
+/*Write function bmi that calculates body mass index (bmi = weight / height2).
+
+if bmi <= 18.5 return "Underweight"
+
+if bmi <= 25.0 return "Normal"
+
+if bmi <= 30.0 return "Overweight"
+
+if bmi > 30 return "Obese"
+
+*/
+
+function bmi(weight, height) {
+    let result = weight/Math.pow(height,2) 
+    
+    if (result <= 18.5) {
+      return "Underweight";
+    } else if (result <= 25) {
+      return "Normal";
+    } else if (result <= 30) {
+      return "Overweight";
+    } else {
+      return "Obese";
+    }
+    
+  }
+
+  //OR
+
+  const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                                    bmi <= 25 ? "Normal" :
+                                    bmi <= 30 ? "Overweight" : "Obese";
+
+//OR
+
+function bmi(weight, height) {
+    const $ = weight / height**2;
+    return $ <= 18.5 ? 'Underweight' : $ <= 25.0 ? 'Normal' : $ <= 30.0 ? 'Overweight' : 'Obese';
+  }
