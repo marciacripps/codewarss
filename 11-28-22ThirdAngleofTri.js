56555e36e90a32ecf116318212ba192a2355f4c1
marciacripps <marciacripps@gmail.com>
2022-11-28 20:51:04 -0500

    8kyu easy

---
+++ 11-28-22ThirdAngleofTri.js
@@ -0,0 +1,16 @@
+/*You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+
+https://en.wikipedia.org/wiki/Triangle*/
+
+function otherAngle(a, b) {
+    return 180-a-b 
+  }
+  
+  //p - two numbers a, b only positive and interior angle numbers 
+  //r - third number for a triangle knowing its   180-a-b
+  //e - (30, 60) 90
+  //p - return 180-a-b 
